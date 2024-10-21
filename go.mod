3e8c7e710898c438584cc5a08c9f11edfa3f855e
kshprenger <kostya@shprenger.com>
2024-10-21 22:01:48 +0300

    Strand tests + deps

--- go.mod
+++ go.mod
@@ -2,27 +2,31 @@ module github.com/bakalover/raft
 
 go 1.22.3
 
-require gorm.io/gorm v1.25.10
-
 require (
-	github.com/golang/protobuf v1.64.0 // indirect
-	github.com/jackc/pgpassfile v1.0.0 // indirect
-	github.com/jackc/pgservicefile v0.0.0-20240606120523-5a60cdf6a761 // indirect
-	github.com/jackc/pgx/v5 v5.6.0 // indirect
-	github.com/jackc/puddle/v2 v2.2.1 // indirect
-	golang.org/x/crypto v0.24.0 // indirect
-	golang.org/x/net v0.26.0 // indirect
-	golang.org/x/sync v0.7.0 // indirect
-	golang.org/x/sys v0.21.0 // indirect
-	golang.org/x/text v0.16.0 // indirect
-	google.golang.org/genproto/googleapis/rpc v0.0.0-20240610135401-a8a62080eff3 // indirect
-	google.golang.org/grpc v1.64.0 // indirect
-	google.golang.org/protobuf v1.34.2 // indirect
+	github.com/stretchr/testify v1.8.1
+	go.uber.org/goleak v1.3.0
+	google.golang.org/grpc v1.67.1
+	google.golang.org/protobuf v1.35.1
+	gorm.io/driver/postgres v1.5.9
+	gorm.io/gorm v1.25.12
 )
 
 require (
-	github.com/bakalover/parlia v0.0.0-20240317202814-93c0d3446291
+	github.com/davecgh/go-spew v1.1.1 // indirect
+	github.com/jackc/pgpassfile v1.0.0 // indirect
+	github.com/jackc/pgservicefile v0.0.0-20221227161230-091c0ba34f0a // indirect
+	github.com/jackc/pgx/v5 v5.5.5 // indirect
+	github.com/jackc/puddle/v2 v2.2.1 // indirect
 	github.com/jinzhu/inflection v1.0.0 // indirect
 	github.com/jinzhu/now v1.1.5 // indirect
-	gorm.io/driver/postgres v1.5.9
+	github.com/kr/text v0.2.0 // indirect
+	github.com/pmezard/go-difflib v1.0.0 // indirect
+	github.com/rogpeppe/go-internal v1.13.1 // indirect
+	golang.org/x/crypto v0.26.0 // indirect
+	golang.org/x/net v0.28.0 // indirect
+	golang.org/x/sync v0.8.0 // indirect
+	golang.org/x/sys v0.24.0 // indirect
+	golang.org/x/text v0.17.0 // indirect
+	google.golang.org/genproto/googleapis/rpc v0.0.0-20240814211410-ddb44dafa142 // indirect
+	gopkg.in/yaml.v3 v3.0.1 // indirect
 )
--- infra/infra_test.go
+++ infra/infra_test.go
@@ -7,6 +7,7 @@ import (
 
 	"github.com/bakalover/raft/infra"
 	"github.com/stretchr/testify/assert"
+	"go.uber.org/goleak"
 )
 
 const (
@@ -39,9 +40,9 @@ func TestQueue(t *testing.T) {
 		q := &infra.Queue{}
 		count := 0
 		var wg sync.WaitGroup
-		for goGroup := 0; goGroup < goGroups; goGroup++ {
+		for range goGroups {
 			wg.Add(gosInGroup)
-			for gos := 0; gos < gosInGroup; gos++ {
+			for range gosInGroup {
 				go func() {
 					defer wg.Done()
 					q.Append(infra.FormNode(func(ctx context.Context) {
@@ -65,8 +66,8 @@ func TestQueue(t *testing.T) {
 		count := 0
 		sig := make(chan struct{})
 		sigMain := make(chan struct{})
-		for goGroup := 0; goGroup < goGroups; goGroup++ {
-			for gos := 0; gos < gosInGroup; gos++ {
+		for range goGroups {
+			for range gosInGroup {
 				go func() {
 					defer func() {
 						sig <- struct{}{}
@@ -105,18 +106,17 @@ func TestQueue(t *testing.T) {
 	})
 }
 
-// TSAN required
 func BenchmarkQueue(b *testing.B) {
 	q := &infra.Queue{}
 	count := 0
 	sig := make(chan struct{})
 	sigMain := make(chan struct{})
-	for gos := 0; gos < gosInGroup; gos++ {
+	for range gosInGroup {
 		go func() {
 			defer func() {
 				sig <- struct{}{}
 			}()
-			for i := 0; i < b.N; i++ {
+			for range b.N {
 				q.Append(infra.FormNode(func(ctx context.Context) {
 					count++
 				}, nil))
@@ -148,4 +148,53 @@ func BenchmarkQueue(b *testing.B) {
 		count = 0
 	}()
 	<-sigMain
+}
+
+func TestStrand(t *testing.T) {
+	t.Run("Just Work", func(t *testing.T) {
+		s := infra.NewStrand(context.Background())
+		crits := 0
+		s.Combine(func(ctx context.Context) {
+			crits++
+		})
+		s.Combine(func(ctx context.Context) {
+			crits++
+		})
+		s.Combine(func(ctx context.Context) {
+			crits++
+		})
+		s.Await()
+		goleak.VerifyNone(t)
+		assert.Equal(t, 3, crits)
+	})
+
+	// TSAN required
+	t.Run("Highload", func(t *testing.T) {
+		s := infra.NewStrand(context.Background())
+		crits := 0
+		for range goGroups {
+			for range gosInGroup {
+				s.Combine(func(ctx context.Context) {
+					crits++
+				})
+			}
+			s.Await()
+			goleak.VerifyNone(t)
+			assert.Equal(t, gosInGroup, crits)
+			crits = 0
+		}
+	})
+}
+
+func BenchmarkStrand(b *testing.B) {
+	s := infra.NewStrand(context.Background())
+	crits := 0
+	for range b.N {
+		s.Combine(func(ctx context.Context) {
+			crits++
+		})
+	}
+	s.Await()
+	assert.Equal(b, b.N, crits)
+	crits = 0
 }
