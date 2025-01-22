3ebbf0f9892ec9c20a4ab672ed43b045838fbf65
kshprenger <kostya@shprenger.com>
2025-01-22 10:26:32 +0300

    Run server

--- balancer/raft.go
+++ balancer/raft.go
@@ -1,30 +1,7 @@
 package main
 
-import (
-	"log"
-	"net/rpc"
-
-	"github.com/bakalover/raft/machine"
-	"github.com/bakalover/raft/raft"
-)
+import "github.com/bakalover/raft/balancer/srv"
 
 func main() {
-	client, err := rpc.DialHTTP("tcp", ":8085")
-	if err != nil {
-		panic(err.Error())
-	}
-	args := machine.RSMcmd{
-		CMD: machine.Add,
-		Xid: machine.Xid{
-			Client: "hiiim",
-			Index:  1,
-		},
-		Arg: 2,
-	}
-	reply := new(raft.RaftReply)
-	err = client.Call("Raft.Apply", args, reply)
-	log.Printf("%+v", reply)
-	if err != nil {
-		panic(err.Error())
-	}
+	srv.NewServer().Start(":11111")
 }
